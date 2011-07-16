98dce88173e35e5e3b327e85188c722ecdbb5634
Dan VerWeire <dverweire@gmail.com>
2011-07-16 00:34:35 -0400

    Changed connect version, styling and new queue method

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 var connect = require('connect'),
-	 fs = require('fs');
+	 fs = require('fs')
+	 spawn = require('child_process').spawn;
 	 
-var index = 0;
+var queue = [], downloading = false;
 
 var server = connect.createServer()
-// .use(
-// 	connect.logger()
-//  )
 .use(
 	connect.query()
 )
@@ -14,13 +12,37 @@ var server = connect.createServer()
 	connect.static(__dirname + '/static')
 )
 .use('/add', function (request, response) {
-	//fs.writeFile(filename, data, encoding='utf8', [callback]
-	index++;
+	response.end('ok');
 	
-	fs.writeFile(__dirname + '/queue/' + index, request.query.url + '\n', function (error) {
-		
-		response.end(JSON.stringify(error));
-		
-	});
+	queue.push(request.query.url);
+	
+	if (!downloading) {
+		downloadVideo(queue.shift());
+	}
 })
 .listen(3000);
+
+function downloadVideo(url) {
+	var proc = spawn('youtube-dl'
+		, ['-t', '--max-quality=37', url]
+	);
+	
+	downloading = true;
+	
+	proc.stdout.on('data', function (data) {
+		console.log('stdout: ' + data);
+	});
+
+	proc.stderr.on('data', function (data) {
+		console.log('stderr: ' + data);
+	});
+
+	proc.on('exit', function (code) {
+		
+		downloading = false;
+		
+		if (queue.length) {
+			downloadVideo(queue.shift());
+		}
+	});
+}
