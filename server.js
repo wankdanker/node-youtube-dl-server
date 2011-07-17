ccfbaa68aa8dbcd4a7f2f2ed6a5dce64b80ea691
Dan VerWeire <dverweire@gmail.com>
2011-07-17 13:31:03 -0400

    now using socket.io for adding urls to the queue, and also for receiving status and queue updates at the web client.

--- server.js
+++ server.js
@@ -1,36 +1,78 @@
+/*
+ * TODO: 
+ * 	- Allow save path from interface
+ * 	- enable all options for youtube-dl
+ * 	- generic download function which then calls functions for specific sites besides youtube / plugin based?
+ * 
+ */
+
 var connect = require('connect'),
 	 fs = require('fs')
 	 spawn = require('child_process').spawn;
 	 
 var queue = [], downloading = false;
 
-var server = connect.createServer()
-.use(
+var downloadPath = "/mnt/media2/Video/Music Videos";
+var status = "Nothing.";
+
+
+var server = connect.createServer();
+var io = require('socket.io').listen(server);
+io.set('log level', 1);
+
+
+server.use(
 	connect.query()
 )
+
 .use(  	
 	connect.static(__dirname + '/static')
 )
-.use('/add', function (request, response) {
-	response.end('ok');
-	
-	queue.push(request.query.url);
-	
-	if (!downloading) {
-		downloadVideo(queue.shift());
-	}
-})
-.listen(3000);
+.listen(3001);
 
-function downloadVideo(url) {
+setInterval(function () {
+	io.sockets.emit('updateStatus', status);
+}, 1000);
+
+io.sockets.on('connection', function (socket) {
+	socket.on('add', function ( request ) {
+		queue.push(request);
+
+		if (!downloading) {
+			downloadFile(queue.shift());
+		}
+		else {
+			io.sockets.emit('updateQueue', queue);
+		}
+	});
+
+	socket.on('ready', function () {
+		socket.emit('updateQueue', queue);
+		socket.emit('updateStatus', status);
+	});
+});
+
+function downloadFile(query) {
+	io.sockets.emit('updateQueue', queue);
+	
+	if (/youtube/gi.test(query.url)) {
+		downloadFromYouTube(query)
+	}
+}
+
+function downloadFromYouTube(query) {
 	var proc = spawn('youtube-dl'
-		, ['-t', '--max-quality=37', url]
+		, ['-t', '--cookies=cookies.txt', '--max-quality=37', query.url],
+		{
+			cwd : downloadPath
+		}
 	);
 	
 	downloading = true;
 	
 	proc.stdout.on('data', function (data) {
 		console.log('stdout: ' + data);
+		status = data.toString();
 	});
 
 	proc.stderr.on('data', function (data) {
@@ -42,7 +84,7 @@ function downloadVideo(url) {
 		downloading = false;
 		
 		if (queue.length) {
-			downloadVideo(queue.shift());
+			downloadFile(queue.shift());
 		}
 	});
 }
